ff5e269d55d52a11a2e40511c7dd40f2e41f2d3a
Michael Susanto <michael.susanto81@ui.ac.id>
2021-06-05 22:19:42 +0700

    add GO

---
+++ webgo/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World from Go")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":8002", nil))
+}
+
+func main() {
+    handleRequests()
+}
